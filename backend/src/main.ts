e2977c0b4917691ed535f7224899660245826571
Jorge Facundo <jorge.facundo.dev@gmail.com>
2026-06-07 18:35:40 -0500

    feat: udpate front logic for backend comunication

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,8 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors();
+
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
